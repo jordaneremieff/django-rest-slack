18d2662898c6d9408d7cbb573ffdbb5dad1fb1b3
erm <jermff@gmail.com>
2018-03-04 19:30:34 +1100

    version

--- setup.py
+++ setup.py
@@ -1,19 +1,15 @@
 import os
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-    README = readme.read()
-
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-rest-slack',
-    version='0.3',
+    version='0.4',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
     description='A simple, reusable Django app for handling and storing Slack events and slash commands.',
-    long_description=README,
     url='https://github.com/erm/django-rest-slack/',
     author='Jordan E.',
     author_email='jermff@gmail.com',
